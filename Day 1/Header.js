576e346a5e2a8f3bbeca41975151935ce1363738
DineshChandra01 <89028912+DineshChandra01@users.noreply.github.com>
2021-08-16 20:28:18 +0530

    Day 1 Assignment Suvmitted

---
+++ Day 1/Header.js
@@ -0,0 +1,17 @@
+
+function Header(){
+
+    let name="Dinesh";
+     return ( 
+         <div>
+        <h1> Header{name} </h1>
+        <p>fill</p>
+
+         </div>
+         
+         
+
+
+     )
+}
+export default Header;
